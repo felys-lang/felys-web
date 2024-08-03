0774d0da45c7e8d266907bd4b5c53b2b69a52673
FelysNeko <88437453+FelysNeko@users.noreply.github.com>
2024-08-03 14:35:18 -0400

    rate limit change

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,17 +1,10 @@
 use std::str::FromStr;
-use std::time::Duration;
 
-use axum::{BoxError, Json, Router};
-use axum::error_handling::HandleErrorLayer;
-use axum::http::{Method, StatusCode};
+use axum::{Json, Router};
 use axum::routing::{get, post};
 use felys::{Language, Worker};
 use serde::{Deserialize, Serialize};
 use tokio::net::TcpListener;
-use tower::buffer::BufferLayer;
-use tower::limit::RateLimitLayer;
-use tower::ServiceBuilder;
-use tower_http::cors::{Any, CorsLayer};
 
 use crate::rsffi::register;
 
@@ -19,25 +12,9 @@ mod rsffi;
 
 #[tokio::main]
 async fn main() {
-    let ignore = |_: BoxError| async {
-        StatusCode::INTERNAL_SERVER_ERROR
-    };
-
-    let cors = CorsLayer::new()
-        .allow_origin(Any)
-        .allow_methods([Method::GET, Method::POST])
-        .allow_headers(Any);
-
-    let ratelimit = ServiceBuilder::new()
-        .layer(HandleErrorLayer::new(ignore))
-        .layer(BufferLayer::new(256))
-        .layer(RateLimitLayer::new(10, Duration::from_secs(1)));
-
     let app = Router::new()
         .route("/", get(|| async { "Felys Web API" }))
-        .route("/execute", post(execute))
-        .layer(ratelimit)
-        .layer(cors);
+        .route("/execute", post(execute));
 
     let listener = TcpListener::bind("0.0.0.0:8000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
