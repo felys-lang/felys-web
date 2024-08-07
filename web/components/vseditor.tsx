5be7f74af29321aa2bf7dc1cf31f6df6cdfd7750
FelysNeko <88437453+FelysNeko@users.noreply.github.com>
2024-08-07 07:27:47 -0400

    update to latest version

--- web/components/vseditor.tsx
+++ web/components/vseditor.tsx
@@ -10,7 +10,7 @@ const config = (_: editor.IStandaloneCodeEditor, monaco: Monaco) => {
     tokenizer: {
       root: [
         [/if|elif|else|while|return|true|false|none|and|or|xor/, "keyword"],
-        [/如果|否如|否则|循环|返回|真|假|无|和|或|异或|大于|小于|等于|大于等于|小于等于|不等于/, "keyword"],
+        [/如果|否如|否则|当|返回|真|假|无|和|或|异或|大于|小于|等于|大于等于|小于等于|不等于/, "keyword"],
 
         [/[a-zA-Z_][\w_]*(?=\s*\()/, "function.call"],
         [/[_\u4e00-\u9fa5][\d_\u4e00-\u9fa5]*(?=\s*（)/, "function.call"],
